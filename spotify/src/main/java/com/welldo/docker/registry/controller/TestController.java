fe383a63014361ac8dfd99887fc0a2f2ba411770
welldo <welldo2012@qq.com>
2022-04-28 18:32:36 +0800

    4. dockerfile-maven插件使用方式

---
+++ spotify/src/main/java/com/welldo/docker/registry/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.welldo.docker.registry.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author:welldo
+ * date: 2022-02-18 13:19
+ */
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test-success";
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "index-success";
+    }
+}
